b30044dae60814389fe464ac202915e7c60668a8
Pedro Henrique Ribeiro da Silva <120214894+PedroXIX@users.noreply.github.com>
2023-04-07 16:48:42 -0300

    Add files via upload

---
+++ maisVelhoClass/pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maisVelhoClass
+{
+    class pessoa
+    {
+        public int idade;
+        public string nome;
+    }
+}
